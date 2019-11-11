458e1151f295253aa77b88e146663e53baa9284b
cubetronic <cubetronic@yahoo.com>
2019-11-11 06:42:50 +0000

    Update 8-user-xfce-settings.sh

--- 8-user-xfce-settings.sh
+++ 8-user-xfce-settings.sh
@@ -27,23 +27,31 @@ xfconf-query \
 -p /panels/panel-1/position \
 -s "p=8;x=683;y=754"
 
+## PREVENT LOADING OF PANEL 2, BY SETTING PANEL ARRAY TO JUST 1
+## THEREBY MAKING PANEL 2 SORT OF NON-EXISTANT
+## BUT IT STILL HAS ALL ITS PROPERTIES, AND CAN BE BROUGHT BACK.
+xfconf-query -c xfce4-panel -p /panels -t int -s 1 -a
+
 ## CHANGE MENU BUTTON (START BUTTON) ICON
 xfconf-query \
 -c xfce4-panel \
 -p /plugins/plugin-1/button-icon \
+-n -t string \
 -s "arch_linux_gnome_menu_icon_by_byamato"
 
 ## CHANGE MENU BUTTON TEXT TO "Menu"
 xfconf-query \
 -c xfce4-panel \
 -p /plugins/plugin-1/button-title \
+-n -t string \
 -s "Menu"
 
 ## DON'T SHOW THAT TEXT NEXT TO MENU BUTTON (START BUTTON) ANYWAY
 xfconf-query \
 -c xfce4-panel \
 -p /plugins/plugin-1/show-button-title \
--s "false"
+-n -t bool \
+-s false
 
-
-
+## RELOAD PANEL(S)
+xfce4-panel -r
