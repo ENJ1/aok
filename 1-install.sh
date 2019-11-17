c73b7ff5745ee5c448e572d848e8ab999fe3104c
cubetronic <cubetronic@yahoo.com>
2019-11-16 22:59:23 -0800

    install images immediately

--- 1-install.sh
+++ 1-install.sh
@@ -69,6 +69,31 @@ install -o root -g root -m 0644 *.txt rootfs/aok/
 install -o root -g root -m 0755 *.sh rootfs/aok/
 cp -r files rootfs/aok
 cp -r extra rootfs/aok
+
+## Actually install images where they should go
+## DEPENDS: files/arch_linux_gnome_menu_icon_by_byamato.png
+## DEPENDS: files/bright_background_light_texture_50370_1366x768.jpg
+
+# INSTALL TEXTURED GREY WALLPAPER
+install -o root -g root -m 0644 -D \
+files/bright_background_light_texture_50370_1366x768.jpg \
+rootfs/usr/share/backgrounds/xfce/bright_background_light_texture_50370_1366x768.jpg
+
+## ARCH LINUX ICON
+install -o root -g root -m 0644 -D \
+files/arch_linux_gnome_menu_icon_by_byamato.png \
+rootfs/usr/share/icons/arch_linux_gnome_menu_icon_by_byamato.png
+
+## LIGHTDM LOGIN BACKGROUND
+install -o root -g root -m 0644 -D \
+files/linux_archlinux_os_blue_black_logo_30861_1366x768.jpg \
+rootfs/usr/share/pixmaps/linux_archlinux_os_blue_black_logo_30861_1366x768.jpg
+
+## LIGHTDM BACKGROUND SETTINGS
+install -o root -g root -m 0644 -D \
+files/home/a/Desktop/aok-nov-7b/files/lightdm-gtk-greeter.conf \
+rootfs/etc/lightdm/lightdm-gtk-greeter.conf
+
 umount rootfs
 sync
 rmdir rootfs
