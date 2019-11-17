840d11ecf3440ee07bf47c40126e9ee02fe6d967
cubetronic <cubetronic@yahoo.com>
2019-11-17 11:18:50 -0800

    added install help to root bashrc

--- 1-install.sh
+++ 1-install.sh
@@ -98,6 +98,15 @@ install -o a -g a -m 0755 -D \
 files/Setup-AOK-Style.sh \
 rootfs/home/a/Desktop
 
+## Edit root bashrc to have welcome message
+echo "dmesg -n 1" >> rootfs/root/.bashrc
+echo "echo ''" >> rootfs/root/.bashrc
+echo "echo 'Welcome. To finish installing AOK, do the following:'"
+echo "echo '1. Type ./2-run-on-first-login.sh and press enter.'" >> rootfs/root/.bashrc
+echo "echo '2. Type wifi-menu and press enter to get online.'" >> rootfs/root/.bashrc
+echo "echo '3. Type ./3-run-when-online.sh and press enter.'" >> rootfs/root/.bashrc
+echo "echo ''" >> rootfs/root/.bashrc
+
 umount rootfs
 sync
 rmdir rootfs
