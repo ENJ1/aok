5f50bcad0853abd6f2c04597ef9d982fa6968162
cubetronic <cubetronic@yahoo.com>
2019-11-16 23:12:57 -0800

    install aok xfce styler to desktop

--- 1-install.sh
+++ 1-install.sh
@@ -77,22 +77,27 @@ cp -r extra rootfs/aok
 # INSTALL TEXTURED GREY WALLPAPER
 install -o root -g root -m 0644 -D \
 files/bright_background_light_texture_50370_1366x768.jpg \
-rootfs/usr/share/backgrounds/xfce/bright_background_light_texture_50370_1366x768.jpg
+rootfs/usr/share/backgrounds/xfce
 
 ## ARCH LINUX ICON
 install -o root -g root -m 0644 -D \
 files/arch_linux_gnome_menu_icon_by_byamato.png \
-rootfs/usr/share/icons/arch_linux_gnome_menu_icon_by_byamato.png
+rootfs/usr/share/icons
 
 ## LIGHTDM LOGIN BACKGROUND
 install -o root -g root -m 0644 -D \
 files/linux_archlinux_os_blue_black_logo_30861_1366x768.jpg \
-rootfs/usr/share/pixmaps/linux_archlinux_os_blue_black_logo_30861_1366x768.jpg
+rootfs/usr/share/pixmaps
 
 ## LIGHTDM BACKGROUND SETTINGS
 install -o root -g root -m 0644 -D \
-files/home/a/Desktop/aok-nov-7b/files/lightdm-gtk-greeter.conf \
-rootfs/etc/lightdm/lightdm-gtk-greeter.conf
+files/lightdm-gtk-greeter.conf \
+rootfs/etc/lightdm
+
+## Now place a script on the Desktop
+install -o a -g a -m 0755 -D \
+files/Setup-AOK-Style.sh \
+rootfs/home/a/Desktop
 
 umount rootfs
 sync
